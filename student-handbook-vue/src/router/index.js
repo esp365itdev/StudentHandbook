c73ad6e69bc3df7b29e34af636bc4cc5d5fa7f5a
77dahuzicat77 <dahuzicat77@163.com>
2025-12-13 09:57:54 +0800

    測試是否能發送學校通知

--- student-handbook-vue/src/router/index.js
+++ student-handbook-vue/src/router/index.js
@@ -1,23 +1,29 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import StudentHandbook from '../components/StudentHandbook.vue'
+import SendNotice from '../views/wechat/SendNotice.vue'
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/handbook',
-    name: 'StudentHandbook',
-    component: StudentHandbook
-  }
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/handbook',
+        name: 'StudentHandbook',
+        component: StudentHandbook
+    },
+    {
+        path: '/wechat/send-notice',
+        name: 'SendNotice',
+        component: SendNotice
+    }
 ]
 
 const router = createRouter({
-  history: createWebHistory('/sp-api/'),
-  routes
+    history: createWebHistory('/sp-api/'),
+    routes
 })
 
 export default router
