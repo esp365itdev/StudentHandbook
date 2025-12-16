d63d3e067ae6d764626e501342666b4ae644fdc5
77dahuzicat77 <dahuzicat77@163.com>
2025-12-16 08:45:59 +0800

    修改配置文件

--- student-handbook-vue/src/router/index.js
+++ student-handbook-vue/src/router/index.js
@@ -1,7 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import StudentHandbook from '../components/StudentHandbook.vue'
-import SendNotice from '../views/wechat/SendNotice.vue'
 
 const routes = [
     {
@@ -13,11 +12,6 @@ const routes = [
         path: '/handbook',
         name: 'StudentHandbook',
         component: StudentHandbook
-    },
-    {
-        path: '/wechat/send-notice',
-        name: 'SendNotice',
-        component: SendNotice
     }
 ]
 
