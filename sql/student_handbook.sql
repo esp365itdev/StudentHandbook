b41a2b05f8ccb839db74a31a75f135693726bcc2
77dahuzicat77 <dahuzicat77@163.com>
2026-01-23 11:53:06 +0800

    1.修改sql文件

--- sql/student_handbook.sql
+++ sql/student_handbook.sql
@@ -1,59 +1,33 @@
--- ----------------------------
--- 学生手册表
--- ----------------------------
-drop table if exists sys_student_handbook;
-create table sys_student_handbook (
-  id                bigint(20)      not null auto_increment    comment '主键ID',
-  subject           varchar(255)    not null                   comment '科目',
-  category          varchar(100)    not null                   comment '类别',
-  start_time        varchar(50)     not null                   comment '开始时间',
-  end_time          varchar(50)     not null                   comment '结束时间',
-  content           text            default null               comment '内容',
-  create_by         varchar(64)     default ''                 comment '创建者',
-  create_time       datetime                                   comment '创建时间',
-  update_by         varchar(64)     default ''                 comment '更新者',
-  update_time       datetime                                   comment '更新时间',
-  primary key (id)
-) engine=innodb auto_increment=1 comment = '学生手册表';
-
--- ----------------------------
--- 初始化-学生手册表数据
--- ----------------------------
-insert into sys_student_handbook values(1, '数学', '功课', '2/9/2025', '2/9/2025', '完成复习题1-10页', 'admin', sysdate(), '', null);
-insert into sys_student_handbook values(2, '语文', '通知', '2/15/2025', '2/15/2025', '下周五学校组织春游，请同学们做好准备', 'admin', sysdate(), '', null);
-insert into sys_student_handbook values(3, '英语', '考试', '3/1/2025', '3/1/2025', '期中考试将于三月第一周举行，请同学们提前复习', 'admin', sysdate(), '', null);
-
 -- 创建class_log表
 DROP TABLE IF EXISTS class_log;
-CREATE TABLE class_log (
-                           id                varchar(64)     NOT NULL                COMMENT '主键ID',
-                           studentClass      varchar(255)    DEFAULT NULL            COMMENT '班级',
-                           teacher           varchar(255)    DEFAULT NULL            COMMENT '教师',
-                           course            varchar(255)    NOT NULL                COMMENT '课程',
-                           courseType        varchar(100)    DEFAULT NULL            COMMENT '课程类型',
-                           content           text            DEFAULT NULL            COMMENT '内容',
-                           startDate         varchar(50)     DEFAULT NULL            COMMENT '开始日期',
-                           endDate           varchar(50)     DEFAULT NULL            COMMENT '结束日期',
-                           updateDate        varchar(50)     DEFAULT NULL            COMMENT '更新日期',
-                           PRIMARY KEY (id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='课程日志表';
+CREATE TABLE `class_log` (
+                             `id` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+                             `studentClass` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+                             `teacher` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+                             `course` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+                             `courseType` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+                             `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+                             `startDate` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+                             `endDate` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+                             `updateDate` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
 
 -- 部门表
 -- ----------------------------
 drop table if exists sys_department;
-create table sys_department (
-                                id                bigint(20)      not null                 comment '部门id',
-                                parent_id         int(11)         default 0                comment '父亲部门id',
-                                name              varchar(255)    not null                 comment '部门名称',
-                                type              int(11)         default 0                comment '部门类型：1-班级, 2-年级, 3-学段, 4-校区, 5-学校',
-                                register_year     int(11)         default null             comment '入学年份',
-                                standard_grade    int(11)         default null             comment '标准年级',
-                                order_num         int(11)         default 0                comment '排序值',
-                                is_graduated      tinyint(1)      default 0                comment '是否毕业：1-是, 0-否',
-                                open_group_chat   tinyint(1)      default 0                comment '是否开启班级群：1-是, 0-否',
-                                group_chat_id     varchar(255)    default null             comment '班级群id',
-                                primary key (id)
-) engine=innodb comment = '部门表';
+CREATE TABLE `sys_department` (
+                                  `id` bigint NOT NULL COMMENT '部门id',
+                                  `parent_id` int DEFAULT '0' COMMENT '父亲部门id',
+                                  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '部门名称',
+                                  `type` int DEFAULT '0' COMMENT '部门类型：1-班级, 2-年级, 3-学段, 4-校区, 5-学校',
+                                  `register_year` int DEFAULT NULL COMMENT '入学年份',
+                                  `standard_grade` int DEFAULT NULL COMMENT '标准年级',
+                                  `order_num` int DEFAULT '0' COMMENT '排序值',
+                                  `is_graduated` tinyint(1) DEFAULT '0' COMMENT '是否毕业：1-是, 0-否',
+                                  `open_group_chat` tinyint(1) DEFAULT '0' COMMENT '是否开启班级群：1-是, 0-否',
+                                  `group_chat_id` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '班级群id',
+                                  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='部门表'
 
 -- 家长学生关系表
 -- ----------------------------
@@ -85,4 +59,19 @@ create table sys_department_parent_binding (
   unique key uk_dept_parent (department_id, parent_user_id)
 ) engine=innodb auto_increment=1 comment = '部门家长绑定表';
 
+-- Token表
+-- ----------------------------
+drop table if exists sys_token;
+CREATE TABLE `sys_token` (
+                             `id` bigint NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+                             `user_id` bigint NOT NULL COMMENT '用户ID',
+                             `parent_user_id` varchar(64) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+                             `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Token值',
+                             `expire_time` datetime NOT NULL COMMENT '过期时间',
+                             `create_time` datetime NOT NULL COMMENT '创建时间',
+                             `update_time` datetime NOT NULL COMMENT '更新时间',
+                             PRIMARY KEY (`id`),
+                             UNIQUE KEY `token_value` (`token`),
+                             KEY `idx_user_id` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=400 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Token表'
 
